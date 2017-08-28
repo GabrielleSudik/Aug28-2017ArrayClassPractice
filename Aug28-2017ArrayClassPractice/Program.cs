b32106f7a0cb73b0bb2469e268103a6e26f0f8a3
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2017-08-28 17:44:09 -0400

    initial commit

---
+++ Aug28-2017ArrayClassPractice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aug28_2017ArrayClassPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //arrays!
+        }
+    }
+}
